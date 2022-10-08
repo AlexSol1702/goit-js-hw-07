4472eaffd65d842cf3fb2ab3f8d32beec6ffa109
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-10-08 11:52:46 +0300

    Task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+let gallery = document.querySelector(".gallery");
+
+
+gallery.insertAdjacentHTML('afterbegin',createItem(galleryItems));
+
+function createItem(array) {return array.map(({preview,original,description}) => {
+  return`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}).join("")};
+
+const modalWindow = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+modalWindow.on('show.simplelightbox', function () {});
+
+
